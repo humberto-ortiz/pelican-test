fa55a72a112670284c3d9a84a25cf7be17ad31f4
Humberto Ortiz-Zuazaga <humberto.ortiz@upr.edu>
2016-02-06 16:42:54 -0400

    Added links to ORCID and Google Scholar profiles

--- pelicanconf.py
+++ pelicanconf.py
@@ -19,10 +19,9 @@ CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 
 # Blogroll
-LINKS = (('UPR', 'http://www.upr.edu/'),
-         ('HPCf', 'http://www.hpcf.upr.edu/'),
-         ('UPR-RP', 'http://www.uprrp.edu/'),
-         ('CCOM', 'http://ccom.uprrp.edu/'),)
+LINKS = (('Google Scholar', 'https://scholar.google.com/citations?user=jlRDnO8AAAAJ&hl=en&oi=ao'),
+         ("ORCID Profile", "//orcid.org/0000-0002-4667-8357")
+)
 
 # Social widget
 SOCIAL = (#('Facebook', 'https://www.facebook.com/humberto.ortiz.zuazaga'),
